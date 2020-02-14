29883bde4082c64edf32be5bae7043419fd815d9
Akash Jeganath <akashjoffl@icloud.com>
2020-02-14 18:37:32 +0530

    159. Advanced Loops

--- Theory/Section 13 Advanced Javascript/untitled.js
+++ Theory/Section 13 Advanced Javascript/untitled.js
@@ -1,113 +1,36 @@
-// Solve the below questions:
-
-// #1 Turn this array into a new array: [1,2,3,[4],[5]]. Bonus if you can do it on one line
-const array = [
-    [1],
-    [2],
-    [3],
-    [
-        [
-            [4]
-        ]
-    ],
-    [
-        [
-            [5]
-        ]
-    ]
-]
-array.flat(2);
-
-
-// #2 Turn this array into a new array: [ 'Hello young grasshopper!', 'you are', 'learning fast!' ]
-const greeting = [
-    ["Hello", "young", "grasshopper!"],
-    ["you", "are"],
-    ["learning", "fast!"]
-];
-
-//Solution:
-console.log(greeting.flatMap(x => x.join(' '))) // [ "Hello young grasshopper!", "you are", "learning fast!" ]
+const basket = ['apple', 'oranges', 'grapes'];
+const detailedBasket = {
+    apples: 5,
+    oranges: 20,
+    grapes: 500
+};
 
 
 
-//#3 Turn the greeting array above into a string: 'Hello young grasshopper you are learning fast!'
-console.log(greeting.flatMap(x => x.join(' ')).join(' ')) // Hello young grasshopper! you are learning fast!
+//1
+for (let i = 0; i < basket.length; i++) {
+    console.log(basket[i]);
+}
 
+//2
+basket.forEach(item => {
+    console.log(item);
+})
 
+// for of
+// iterating - able to go one by one (arrays, strings)
+for (item of basket) {
+    console.log(item);
+}
 
-//#4 Turn the trapped 3 number into: [3]
-const trapped = [
-    [
-        [
-            [
-                [
-                    [
-                        [
-                            [
-                                [
-                                    [
-                                        [
-                                            [
-                                                [
-                                                    [
-                                                        [
-                                                            [
-                                                                [
-                                                                    [
-                                                                        [
-                                                                            [
-                                                                                [
-                                                                                    [
-                                                                                        [
-                                                                                            [
-                                                                                                [
-                                                                                                    [3]
-                                                                                                ]
-                                                                                            ]
-                                                                                        ]
-                                                                                    ]
-                                                                                ]
-                                                                            ]
-                                                                        ]
-                                                                    ]
-                                                                ]
-                                                            ]
-                                                        ]
-                                                    ]
-                                                ]
-                                            ]
-                                        ]
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
-        ]
-    ]
-];
+// basket = {
+//     0: 'apples',
+//     1: 'oranges',
+//     2: 'grapes'
+// }
 
-console.log(trapped.flat(Infinity)) // Array [ 3 ]
-
-//#5 Clean up this email to have no whitespaces. Make the answer be in a single line (return a new string):
-const userEmail3 = '     cannotfillemailformcorrectly@gmail.com   '
-
-console.log(userEmail3.trimEnd().trimStart()) // cannotfillemailformcorrectly@gmail.com
-
-
-//#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
-const users = { user1: 18273, user2: 92833, user3: 90315 }
-
-const usersArray = Object.entries(users) // [ (2) […], (2) […], (2) […] ]
-
-//#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
-updatedUsersArray = usersArray.map((user) => [user[0], user[1] * 2])
-    /* 0: Array [ "user1", 36546 ]    ​
-    1: Array["user2", 185666]​
-    2: Array["user3", 180630] */
-
-//#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
-
-const newerUsers = Object.fromEntries(updatedUsersArray); //  { user1: 36546, user2: 185666, user3: 180630 }
+// for in
+// enumerating - objects
+for (item in detailedBasket) {
+    console.log(item);
+}
