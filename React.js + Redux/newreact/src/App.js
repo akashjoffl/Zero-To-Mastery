a9b2adc2197ec4f4214381cd5a4c4a0871853954
Akash Jeganath <akashjoffl@icloud.com>
2020-03-05 00:16:57 +0530

    React component

--- React.js + Redux/newreact/src/App.js
+++ React.js + Redux/newreact/src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Example1 extends React.Component {
+  constructor(props) {
+    this.customFunction = this.customFunction.bind(this);
+  }
+}
+
+class Example2 extends React.Component {
+  render() {
+    return (
+      <div>
+      <p>Hello World!</p>
+      </div>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -24,3 +40,5 @@ function App() {
 }
 
 export default App;
+export default Example1;
+export default Example2;
