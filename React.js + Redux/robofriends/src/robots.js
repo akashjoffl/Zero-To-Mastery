7679abe678a17f74a7a54a87e00cdeb3a65a1e30
Akash Jeganath <akashjoffl@icloud.com>
2020-03-05 02:58:52 +0530

    added extra robots

--- React.js + Redux/robofriends/src/robots.js
+++ React.js + Redux/robofriends/src/robots.js
@@ -58,6 +58,18 @@ export const robots = [
       name: 'Clementina DuBuque',
       username: 'Moriah.Stanton',
       email: 'Rey.Padberg@karina.biz'
+    },
+    {
+      id: 11,
+      name: 'Nadhan James',
+      username: 'Nadhan.james',
+      email: 'Nadha.James@katty.io'
+    },
+    {
+      id: 12,
+      name: 'Mickey Bobs',
+      username: 'Mickey.Bobs',
+      email: 'Mickey.Bobs@kissy.com'
     }
   ];
   
