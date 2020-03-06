ba0ed2d791a995ec38196c9399d38d3824f718e7
Akash Jeganath <akashjoffl@icloud.com>
2020-03-06 11:58:03 +0530

    comment

--- React.js + Redux/robofriends/src/index.js
+++ React.js + Redux/robofriends/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { robots } from './robots';
+import { robots } from './robots'; // { } destructing since it's exporting 
 
 ReactDOM.render(
    <CardList robots={robots}/>
