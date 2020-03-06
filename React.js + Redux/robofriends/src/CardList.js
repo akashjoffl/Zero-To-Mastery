3265c81a29995dfdbf9badf146a7c09e1a8e8bf3
Akash Jeganath <akashjoffl@icloud.com>
2020-03-06 11:53:23 +0530

    comment

--- React.js + Redux/robofriends/src/CardList.js
+++ React.js + Redux/robofriends/src/CardList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-    return (
+    return ( // return the title later 
         <div>
             {robots.map((user, i) => {
                 return (
