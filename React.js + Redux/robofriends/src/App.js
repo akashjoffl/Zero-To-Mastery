e4ae5ceb962bbe3e67a4c451d61665e2c601659f
Akash Jeganath <akashjoffl@icloud.com>
2020-03-06 17:05:21 +0530

    implemented searchBox with records log

---
+++ React.js + Redux/robofriends/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import { robots } from './robots';
+import SearchBox from './SearchBox';
+import { render } from 'react-dom';
+
+class App extends Component {
+    constructor() {
+        super() // weird thing to add here for constructor before this. 
+        this.state = {
+            robots: robots,
+            searchfield: ""
+        }
+    }
+
+    onSearchChange(event) {
+        console.log(event.target.value);
+    }
+    render() {
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/> 
+                <CardList robots={this.state.robots} // weird react syntax (this.state) robot passed down as props
+                /> 
+            </div>
+        );
+    }
+ };
+
+
+export default App;
--- React.js + Redux/robofriends/src/index.js
+++ React.js + Redux/robofriends/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList'; // forLoop card names
+import App from './App'; // forLoop card names
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots'; // { } destructing since it's exporting 
 
-ReactDOM.render(
-   <CardList robots={robots}/>
-    , document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
     
 serviceWorker.unregister();
