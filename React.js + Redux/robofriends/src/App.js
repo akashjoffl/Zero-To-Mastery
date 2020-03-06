aaa82745a4949a0de436417760da88f88b89d058
Akash Jeganath <akashjoffl@icloud.com>
2020-03-07 01:33:16 +0530

    210. Building A React App 3

--- React.js + Redux/robofriends/src/App.js
+++ React.js + Redux/robofriends/src/App.js
@@ -15,17 +15,17 @@ class App extends Component {
 
     onSearchChange = (event) => { // anytime comes from React (onstructor, rendor) making method use arrow functions #weirdReact  
         this.setState( { searchfield: event.target.value })
+        
+    }
+    render() {
         const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        console.log(filteredRobots);
-    }
-    render() {
         return (
             <div className="tc">
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/> 
-                <CardList robots={this.state.robots} // weird react syntax (this.state) robot passed down as props
+                <CardList robots={filteredRobots} // weird react syntax (this.state) robot passed down as props
                 /> 
             </div>
         );
