961a408dec2474f29e3277e7af6944c36f4cf397
Akash Jeganath <akashjoffl@icloud.com>
2020-03-06 12:02:53 +0530

    comment

--- React.js + Redux/robofriends/src/Card.js
+++ React.js + Redux/robofriends/src/Card.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt="robots" src={`https://robohash.org/${id}?200*200`} />
+            <img alt="robots" src={`https://robohash.org/${id}?200*200`} // template string randomizing URL
+            /> 
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
