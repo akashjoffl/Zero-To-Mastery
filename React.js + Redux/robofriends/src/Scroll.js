3c7020278241c4fc9a39cdee9ac8732aa77a567f
Akash Jeganath <akashjoffl@icloud.com>
2020-03-08 00:40:16 +0530

    7:00 support scroll

--- React.js + Redux/robofriends/src/Scroll.js
+++ React.js + Redux/robofriends/src/Scroll.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Scroll = () => {
-    return
+const Scroll = (props) => {
+    return (
+        <div style={{overflowY: 'scroll', border: '1px solid black', height: '500px'}}>
+            {props.children}
+        </div>
+    )
 };
 
 
