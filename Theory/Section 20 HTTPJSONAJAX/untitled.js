c4e77338d213e5ee06e12dd097947b2a47d3fba0
Akash Jeganath <akashjoffl@icloud.com>
2020-03-18 04:40:09 +0530

    239. Exercise: ES8 - Async Await

--- Theory/Section 20 HTTPJSONAJAX/untitled.js
+++ Theory/Section 20 HTTPJSONAJAX/untitled.js
@@ -1,33 +1,33 @@
-// ASYNC AWAIT
+// Solve the below problems:
 
-// 8:40
+// #1) Convert the below promise into async await
+fetch('https://swapi.co/api/starships/9/')
+  .then(response => response.json())
+  .then(console.log)
 
-async function fetchUsers() {
-    const resp = await fetch('https://jsonplaceholder.typicode.com/todos')
-    const data = await resp.json();
-    // .then(resp => resp.json())
-    console.log(data);
-}
 
-// EXAMPLE 2
-
+// #2) ADVANCED: Update the function below from the video to also have
+// async await for this line: fetch(url).then(resp => resp.json())
+// So there shouldn't be any .then() calls anymore!
+// Don't get discouraged... this is a really tough one...
 const urls = [
-    'https://jsonplaceholder.typicode.com/todos',
-    'https://jsonplaceholder.typicode.com/users',
-    'https://jsonplaceholder.typicode.com/posts'
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholder.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums'
 ]
 
 const getData = async function() {
-    try {
-        const [ todos, users, posts ] = await Promise.all(urls.map(url =>
-            fetch(url).then(resp => resp.json())
-        ))
-        console.log('todos', todos)
-        console.log('users', users)
-        console.log('posts', posts)
-    } catch (err){
-        console.log('Oops', err)
-    }
+  const [ users, posts, albums ] = await Promise.all(urls.map(url =>
+      fetch(url).then(resp => resp.json())
+  ));
+  console.log('users', users);
+  console.log('posta', posts);
+  console.log('albums', albums);
 }
 
-// getData()
+// #3)Add a try catch block to the #2 solution in order to catch any errors. // Now, use the given array containing an invalid url, so you console.log  //your error with 'oooooops'.
+const urls = [
+  'https://jsonplaceholder.typicode.com/users',
+  'https://jsonplaceholdeTYPO.typicode.com/posts',
+  'https://jsonplaceholder.typicode.com/albums'
+]
