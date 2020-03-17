05cdd561804a7fc4bcb143c7e912eb8875972436
Akash Jeganath <akashjoffl@icloud.com>
2020-03-18 04:39:13 +0530

    238. ES8 - Async Await

--- Theory/Section 20 HTTPJSONAJAX/untitled.js
+++ Theory/Section 20 HTTPJSONAJAX/untitled.js
@@ -1,57 +1,33 @@
-// Solve the questions below:
+// ASYNC AWAIT
 
-// #1) Create a promise that resolves in 4 seconds and returns "success" string
+// 8:40
 
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('success');
-    }, 4000)
-}); 
+async function fetchUsers() {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/todos')
+    const data = await resp.json();
+    // .then(resp => resp.json())
+    console.log(data);
+}
 
-promise.then(result => console.log(result));
+// EXAMPLE 2
 
-
-// #2) Run the above promise and make it console.log "success"
-promise.then(console.log);
-
-// #3) Read about Promise.resolve() and Promise.reject(). How can you make
-// the above promise shorter with Promise.resolve() and console loggin "success"
-const promise = Promise.resolve(
-    setTimeout(() => {
-        console.log("Success");
-    }, 4000)
-);
-
-// #4) Catch this error and console log 'Ooops something went wrong'
-//Promise.reject('failed')
-
-const promise = new Promise((resolve, reject) => {
-    promise.reject('failed')
-    setTimeout(resolve, 4000, 'success')
-}).catch(() => console.log("Ooops something went wrong"));
-
-
-
-
-// #5) Use Promise.all to fetch all of these people from Star Wars (SWAPI) at the same time.
-// Console.log the output and make sure it has a catch block as well.
 const urls = [
-  'https://swapi.co/api/people/1',
-  'https://swapi.co/api/people/2',
-  'https://swapi.co/api/people/3',
-  'https://swapi.co/api/people/4'
+    'https://jsonplaceholder.typicode.com/todos',
+    'https://jsonplaceholder.typicode.com/users',
+    'https://jsonplaceholder.typicode.com/posts'
 ]
 
-Promise.all(urls.map(url => {
-    return fetch(url).then(resp => resp.json())
-})).then(result => {
-    console.log(result[0])
-    console.log(result[1])
-    console.log(result[2])
-    console.log(result[3])
-}).catch(() => console.log("Error!"))
+const getData = async function() {
+    try {
+        const [ todos, users, posts ] = await Promise.all(urls.map(url =>
+            fetch(url).then(resp => resp.json())
+        ))
+        console.log('todos', todos)
+        console.log('users', users)
+        console.log('posts', posts)
+    } catch (err){
+        console.log('Oops', err)
+    }
+}
 
-// #6) Change one of your urls above to make it incorrect and fail the promise
-// does your catch block handle it?
-
-// Yes! -> Error!
+// getData()
