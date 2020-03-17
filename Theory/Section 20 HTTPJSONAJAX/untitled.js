1d5be629d5fd2d13ac36a6369621624c56f296dd
Akash Jeganath <akashjoffl@icloud.com>
2020-03-18 04:08:23 +0530

    237. Solutions: Promises

--- Theory/Section 20 HTTPJSONAJAX/untitled.js
+++ Theory/Section 20 HTTPJSONAJAX/untitled.js
@@ -2,15 +2,36 @@
 
 // #1) Create a promise that resolves in 4 seconds and returns "success" string
 
-// #2) Run the above promise and make it console.log "success"
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('success');
+    }, 4000)
+}); 
 
+promise.then(result => console.log(result));
+
+
+// #2) Run the above promise and make it console.log "success"
+promise.then(console.log);
 
 // #3) Read about Promise.resolve() and Promise.reject(). How can you make
 // the above promise shorter with Promise.resolve() and console loggin "success"
-
+const promise = Promise.resolve(
+    setTimeout(() => {
+        console.log("Success");
+    }, 4000)
+);
 
 // #4) Catch this error and console log 'Ooops something went wrong'
-Promise.reject('failed')
+//Promise.reject('failed')
+
+const promise = new Promise((resolve, reject) => {
+    promise.reject('failed')
+    setTimeout(resolve, 4000, 'success')
+}).catch(() => console.log("Ooops something went wrong"));
+
+
+
 
 // #5) Use Promise.all to fetch all of these people from Star Wars (SWAPI) at the same time.
 // Console.log the output and make sure it has a catch block as well.
@@ -21,5 +42,16 @@ const urls = [
   'https://swapi.co/api/people/4'
 ]
 
+Promise.all(urls.map(url => {
+    return fetch(url).then(resp => resp.json())
+})).then(result => {
+    console.log(result[0])
+    console.log(result[1])
+    console.log(result[2])
+    console.log(result[3])
+}).catch(() => console.log("Error!"))
+
 // #6) Change one of your urls above to make it incorrect and fail the promise
 // does your catch block handle it?
+
+// Yes! -> Error!
