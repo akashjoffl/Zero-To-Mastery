54d14d21d6b8a3a3b76356249b41512bd6807d19
Akash Jeganath <akashjoffl@icloud.com>
2020-03-20 01:35:11 +0530

    240. ES9 (ES2018)

--- Theory/Section 20 HTTPJSONAJAX/untitled.js
+++ Theory/Section 20 HTTPJSONAJAX/untitled.js
@@ -1,62 +1,19 @@
-// Solve the below problems:
+// Object Spread operator
 
-// #1) Convert the below promise into async await
-fetch('https://swapi.co/api/starships/9/')
-  .then(response => response.json())
-  .then(console.log)
-
-async function fetchStarship() {
-  const response = await fetch('https://swapi.co/api/starships/9/')
-  const data = await response.json();
-  console.log(data);
+const animals = {
+  tiger: 8, 
+  lion: 5,
+  monkey: 2,
+  bird: 40
 }
 
-// #2) ADVANCED: Update the function below from the video to also have
-// async await for this line: fetch(url).then(resp => resp.json())
-// So there shouldn't be any .then() calls anymore!
-// Don't get discouraged... this is a really tough one...
-const urls = [
-  'https://jsonplaceholder.typicode.com/users',
-  'https://jsonplaceholder.typicode.com/posts',
-  'https://jsonplaceholder.typicode.com/albums'
-]
 
-const getData = async function() {
-  const [ users, posts, albums ] = await Promise.all(urls.map(url =>
-      fetch(url).then(resp => resp.json())
-  ));
-  console.log('users', users);
-  console.log('posta', posts);
-  console.log('albums', albums);
+function objectSpread(p1, p2, p3) {
+  console.log(p1)
+  console.log(p2)
+  console.log(p3)
 }
 
-const getData = async function() {
-  const [ users, posts, albums ] = await Promise.all(urls.map(async function(url) {
-      const response = await fetch(url);
-      return response.json();
-  }));
-  console.log('users', users);
-  console.log('posta', posts);
-  console.log('albums', albums);
-}
+const { tiger, lion, ...rest } = animals;
 
-// #3)Add a try catch block to the #2 solution in order to catch any errors. // Now, use the given array containing an invalid url, so you console.log  //your error with 'oooooops'.
-const urls = [
-  'https://jsonplaceholder.typicode.com/users',
-  'https://jsonplaceholdeTYPO.typicode.com/posts',
-  'https://jsonplaceholder.typicode.com/albums'
-]
-
-const getData = async function() {
-  try {
-    const [ users, posts, albums ] = await Promise.all(urls.map(async function(url) {
-        const response = await fetch(url);
-        return response.json();
-    }));
-    console.log('users', users);
-    console.log('posta', posts);
-    console.log('albums', albums);
-  } catch (err) {
-    console.log('ooooooops', err);
-  }
-}
+objectSpread(tiger, lion, rest)
