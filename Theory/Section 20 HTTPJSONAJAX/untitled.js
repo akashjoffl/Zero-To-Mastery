fa512c150c547ff8b28eb42b70289fc9a9c622c8
Akash Jeganath <akashjoffl@icloud.com>
2020-03-21 00:42:16 +0530

    CodeSmite: Software Engineering Immersive Application

--- Theory/Section 20 HTTPJSONAJAX/untitled.js
+++ Theory/Section 20 HTTPJSONAJAX/untitled.js
@@ -1,83 +1,28 @@
-restParam(1, 2, 3, 4, 5);
+// Challenge: timesBy
 
-function restParam(p1, p2, ...p3) {
-  // p1 = 1
-  // p2 = 2
-  // p3 = [3, 4, 5]
-}
-
-
-const myObject = {
-  a: 1,
-  b: 2,
-  c: 3
-};
-
-const { a, ...x } = myObject;
-// a = 1
-// x = { b: 2, c: 3 }
-
-//finally
-const urls = [
-  'https://swapi.co/api/people/1',
-  'https://swapi.co/api/people/2',
-  'https://swapi.co/api/people/3',
-  'https://swapi.co/api/people/4'
-]
-
-Promise.all(urls.map(url => fetch(url).then(people => people.json())))
-  .then(array => {
-    throw Error
-    console.log('1', array[0])
-    console.log('2', array[1])
-    console.log('3', array[2])
-    console.log('4', array[3])
-  })
-  .catch(err => console.log('ughhhh fix it!', err))
-  .finally(() => console.log('extra action here'))
+// Define a function timesBy which takes in a number n as an argument. 
+// timesBy should return a function that can take an array of numbers as an argument. 
+// The returned function should map over the array and multiply each array value by N.
 
 
 
-
-const urls = [
-  'https://jsonplaceholder.typicode.com/users',
-  'https://jsonplaceholder.typicode.com/postss',
-  'https://jsonplaceholder.typicode.com/albums'
-]
-
-const getData = async function() {
-  try {
-    const [ users, posts, albums ] = await Promise.all(urls.map(async function(url) {
-        const response = await fetch(url);
-        return response.json();
-    }));
-    console.log('users', users);
-    console.log('posts', posts);
-    console.log('albums', albums);
-  } catch (err) {
-    console.log('ooooooops', err);
-  }
-}
-
-//New for await of feature:
-const loopThroughUrl = (urls) => {
-  for (url of urls) {
-    console.log(url)
-  }
-}
-
-
-const getData2 = async function() {
-  const arrayOfPromises = urls.map(url => fetch(url));
-  for (const request of arrayOfPromises) {
-    console.log(request);
-  }
-
-  for await (const request of arrayOfPromises) {
-    const data = await request.json(); 
-    console.log(data)
-  }
-  //In this case, for-await takes each item from the array and waits for it to resolve. You'll get the first response even if the second response isn't ready yet, but you'll always get the responses in the correct order.
-}
+const timesBy = (N) => {
+  function newarr(arr) {
+      let result = [];
+      result = arr.map(function(x) { 
+        return x * N; 
+      });
+      return result; 
+    }
+    return newarr;
+  };
+  
+  /*
+   * Uncomment the following lines and Run Code to test your work
+   */
+  const timesBy2 = timesBy(2); 
+  const timesBy3 = timesBy(3); 
+  timesBy2([1, 3, 5, 7, 9]); // -> [2, 6, 10, 14, 18]
+  timesBy3([1, 3, 5, 7, 9]); // -> [3, 9, 15, 21, 27]
 
 
